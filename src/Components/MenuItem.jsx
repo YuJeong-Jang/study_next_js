f2f0accdde483225ab8929e8cf0542718ea1638d
YuJeong-Jang <aridoasis@naver.com>
2024-08-20 12:11:35 +0900

    movie data fetching - mainpage

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,17 +1,16 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-export default function MenuItem() {
+export default function MenuItem({ route, title }) {
   return (
     <li>
-    <Link
-      href="/"
-      className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-      aria-current="page"
-    >
-      Home
-    </Link>
-  </li>
-  
-  )
+      <Link
+        href={route}
+        className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+        aria-current="page"
+      >
+        {title}
+      </Link>
+    </li>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,33 @@
+// tailwind css bootstrap => flowbite
+// movie api website => tmdb
 
-export default function Home() {
+// next14에서 작성한 모든 컴포넌트 (App 디렉토리 내의 컴포넌트)
+export default async function Home() {
+  // 1. imdb에서 데이터 페칭 수행 --> movie 데이터 json 파일 응담
+  // 2. json.results --> []이 응답 .. 20개의 영화 데이터를 포함
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDM3YTAwN2ZhMmE5MzM1NTdmNWYyMzBlMGYyZTYwZiIsIm5iZiI6MTcyNDEyMTgzNS45Mzg1OTEsInN1YiI6IjY0OTkzOGVkNmY0M2VjMDBjNWM3MmY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lKLi0NyV3qV0XNsDaZbeHw6IWrAjDam7TWu0nq9wxnc",
+    },
+  };
+
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1&region=kr",
+    options
+  );
+  const json = await response.json();
+  const movies = json.results; // 데이터 확보
+  console.log(movies);
+
   return (
-    // tailwind css bootstrap => flowbite
-<div>
-  <h1>Hello Next</h1>
-</div>
+    <div>
+      {movies.map((movie) => {
+        return <div key={movie.title}>{movie.title}</div>;
+      })}
+    </div>
   );
 }
